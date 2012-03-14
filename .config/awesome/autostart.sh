1d778d57cff112cbd041d6393259398aa6adf452
pylemon <leeway1985@gmail.com>
2012-03-14 15:11:57 +0800

    re init

--- .config/awesome/autostart.sh
+++
@@ -1,4 +0,0 @@
-AWESOME_CONFIG_PATH=$HOME/.config/awesome
-
-export LC_CTYPE="zh_CN.UTF-8"
-export LANG="zh_CN.UTF-8"
--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 # install gitconfig
 rm /home/liwei/.gitconfig
-ln -s /home/liwei/dotfiles/.gitconfig /home/liwei/.gitconfig
+ln -s /home/liwei/dotfiles/gitconfig /home/liwei/.gitconfig
 echo 'setting up .gitconfig...'
