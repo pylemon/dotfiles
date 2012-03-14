b592d6fd29346c7a5072085dbbfd77b72385d500
pylemon <leeway1985@gmail.com>
2012-03-14 15:37:00 +0800

    change install mode to cp

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 # install gitconfig
-rm /home/liwei/.gitconfig
-ln -s /home/liwei/dotfiles/gitconfig /home/liwei/.gitconfig
-echo 'setting up .gitconfig...'
+
+rm -f /home/liwei/.gitconfig
+cp -f /home/liwei/dotfiles/gitconfig /home/liwei/.gitconfig
+
+echo 'setting up .gitconfig ...'
+
+
+# install vimperator
+
+rm -f /home/liwei/.vimperatorrc
+cp -f /home/liwei/dotfiles/vimperatorrc /home/liwei/.vimperatorrc
+
+rm -rf /home/liwei/.vimperator
+cp -rf /home/liwei/dotfiles/vimperator /home/liwei/.vimperator
+
+echo 'setting up .vimperator ...'
