7e2c7529802efe927adf4961001937a5cf0e9208
pylemon <leeway1985@gmail.com>
2012-12-20 22:38:23 +0800

    Update system_bootstrap.sh
    add feh. change $HOME to ~

--- system_bootstrap.sh
+++ system_bootstrap.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # useful tools
-sudo apt-get -y install htop tilda tmux git mg zsh nginx apache2-utils awesome awesome-extra suckless-tools lxterminal fcitx-googlepinyin
+sudo apt-get -y install htop tilda tmux feh git mg zsh nginx apache2-utils awesome awesome-extra suckless-tools lxterminal fcitx-googlepinyin
 
 # python dependence
 sudo apt-get -y install python-dev build-essential python-pip
@@ -28,34 +28,34 @@ else
 fi
 
 # install dotfiles
-if [ ! -d $HOME/dotfiles ]; then
-    cd $HOME/
+if [ ! -d ~/dotfiles ]; then
+    cd ~/
     git clone git@github.com:pylemon/dotfiles.git
-    cd $HOME/dotfiles
+    cd ~/dotfiles
     git submodule init
     git submodule update
-    cd $HOME/
-    if [ ! -d $HOME/.zshrc ]; then
-	mv $HOME/.zshrc $HOME/.zshrc.`date +%s`
+    cd ~/
+    if [ ! -d ~/.zshrc ]; then
+	mv ~/.zshrc ~/.zshrc.`date +%s`
     fi
-    if [ ! -d $HOME/.dircolors ]; then
-	mv $HOME/.dircolors $HOME/.dircolors.`date +%s`
+    if [ ! -d ~/.dircolors ]; then
+	mv ~/.dircolors ~/.dircolors.`date +%s`
     fi
-    if [ ! -d $HOME/.gitconfig ]; then
-	mv $HOME/.gitconfig $HOME/.gitconfig.`date +%s`
+    if [ ! -d ~/.gitconfig ]; then
+	mv ~/.gitconfig ~/.gitconfig.`date +%s`
     fi
-    if [ ! -d $HOME/.gitignore_global ]; then
-	mv $HOME/.HOME/.gitignore_global $HOME/.HOME/.gitignore_global.`date +%s`
+    if [ ! -d ~/.gitignore_global ]; then
+	mv ~/.gitignore_global ~/.gitignore_global.`date +%s`
     fi
 
-    ln -s $HOME/dotfiles/_zshrc $HOME/.zshrc
-    ln -s $HOME/dotfiles/_dircolors $HOME/.dircolors
-    ln -s $HOME/dotfiles/_gitconfig $HOME/.gitconfig
-    ln -s $HOME/dotfiles/_gitignore_global $HOME/.gitignore_global
+    ln -s ~/dotfiles/_zshrc ~/.zshrc
+    ln -s ~/dotfiles/_dircolors ~/.dircolors
+    ln -s ~/dotfiles/_gitconfig ~/.gitconfig
+    ln -s ~/dotfiles/_gitignore_global ~/.gitignore_global
 fi
 
 # virtualenv wrapper
 sudo pip install virtualenvwrapper
-if [ ! -d $HOME/Envs ]; then
-    mkdir $HOME/Envs
+if [ ! -d ~/Envs ]; then
+    mkdir ~/Envs
 fi
