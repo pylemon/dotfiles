2d4a55c07cf9815b4ec5c7db151a8d289e49e219
pylemon <leeway1985@gmail.com>
2015-05-08 19:57:59 +0800

    add gtkrc

--- _zshrc.sh
+++ _zshrc.sh
@@ -155,3 +155,5 @@ compctl -g '~/.teamocil/*(:t:r)' teamocil
 
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+xmodmap ~/.Xmodmap
